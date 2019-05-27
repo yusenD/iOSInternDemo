16e30b7b6a2c1816f4eb09b48da39aa69ed77287
jingsong.jn <jingsong.jn@alibaba-inc.com>
2019-05-27 10:56:59 +0800

    init commit

---
+++ iOSInternDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSInternDemo
+//
+//  Created by jingsong.jn on 2019/5/27.
+//  Copyright © 2019 jingsong.jn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSInternDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSInternDemo
+//
+//  Created by jingsong.jn on 2019/5/27.
+//  Copyright © 2019 jingsong.jn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
